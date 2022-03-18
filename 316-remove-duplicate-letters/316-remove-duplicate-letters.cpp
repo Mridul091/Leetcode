bdaac7e6cdede32edf824f42ec7658f35b68fdf5
Mridul Bagoria <64253896+Mridul091@users.noreply.github.com>
2022-03-18 15:48:56 +0530

    Time: 3 ms (73.49%), Space: 6.7 MB (52.77%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<bool> seen(26,false);
+        
+        vector<int> last_ind(26,0);
+        for(int i=0;i<s.length();i++){
+            last_ind[s[i]-'a'] = i;
+        }
+        
+        stack<int> st;
+        for(int i=0;i<s.size();i++){
+            int curr = s[i]-'a';
+            if(seen[curr])
+                continue;
+            
+            while(st.size()>0 && st.top() > s[i] &&  i<last_ind[st.top()-'a']){
+                seen[st.top()-'a']=false;
+                st.pop();
+                }
+            st.push(s[i]);
+            seen[curr]=true;
+        }
+        string res="";
+        while(st.size()>0){
+            res+=st.top();
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
