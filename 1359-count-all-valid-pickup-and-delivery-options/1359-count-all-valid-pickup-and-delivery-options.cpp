6f02f8288fb6a320da5aa3fb74f1e47fa285b093
Mridul Bagoria <64253896+Mridul091@users.noreply.github.com>
2022-03-25 11:19:10 +0530

    Time: 4 ms (32.79%), Space: 5.8 MB (77.64%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -6,7 +6,7 @@ public:
             return 1;
         
         long long ans = (long long)n*(2*n-1) % mod;
-        ans = ans*countOrders(n-1)%mod;
+        ans = ans * countOrders(n-1)%mod;
         return (int) ans;
     }
 };
