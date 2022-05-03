3dbc4e9ba7d777456b5c3573e6e22a7bb94c4d63
Mridul Bagoria <64253896+Mridul091@users.noreply.github.com>
2022-05-03 07:13:29 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (10.45%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    int helper(int n, vector<int> &dp){
+        if(n<=1) return n;
+        
+        if(dp[n]!=-1) return dp[n];
+        
+        return dp[n] = helper(n-1, dp) + helper(n-2, dp);
+    }
+    
+    int fib(int n) {
+//         if(n<=1)
+//             return n;
+        
+//         return fib(n-1) + fib(n-2);
+        
+        vector<int> dp(n+1,-1);
+        int ans =  helper(n, dp);
+        return ans;
+    }
+};
