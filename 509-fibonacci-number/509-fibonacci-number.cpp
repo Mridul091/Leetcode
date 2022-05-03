d4e266ea48a094eaa53c939ff1f63825e49f7a7e
Mridul Bagoria <64253896+Mridul091@users.noreply.github.com>
2022-05-03 07:24:21 +0530

    Time: 3 ms (55.96%), Space: 5.9 MB (42.28%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -10,13 +10,26 @@ public:
     }
     
     int fib(int n) {
+        // recursive solution
 //         if(n<=1)
 //             return n;
         
 //         return fib(n-1) + fib(n-2);
+        // Memoization solution
+        // vector<int> dp(n+1,-1);
+        // int ans =  helper(n, dp);
+        // return ans;
         
-        vector<int> dp(n+1,-1);
-        int ans =  helper(n, dp);
-        return ans;
+//         tabulation with space optimization
+        
+        int prev2 = 0;
+        int prev = 1;
+        if(n==0) return 0;
+        for(int i= 2;i<=n;i++){
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
     }
 };
