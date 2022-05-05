00820762a4a67e28a66660633e43ca4a741bf59b
Mridul Bagoria <64253896+Mridul091@users.noreply.github.com>
2022-05-05 07:18:20 +0530

    Time: 10 ms (5.48%), Space: 7.8 MB (26.33%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     
-    int helper(int ind, vector<int> &nums, vector<int> &dp){
-        if(ind == 0) return nums[ind];
-        if(ind<0) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int left = nums[ind] + helper(ind-2, nums, dp);
-        cout << left << endl;
-        int right = helper(ind-1, nums ,dp);
+//     int helper(int ind, vector<int> &nums, vector<int> &dp){
+//         if(ind == 0) return nums[ind];
+//         if(ind<0) return 0;
+//         if(dp[ind]!=-1) return dp[ind];
+//         int left = nums[ind] + helper(ind-2, nums, dp);
+//         int right = helper(ind-1, nums ,dp);
         
-        return dp[ind] = max(left, right);
-    }
+//         return dp[ind] = max(left, right);
+//     }
     
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        return helper(n-1, nums, dp);
+        vector<int> dp(n, 0);
+        // return helper(n-1, nums, dp);
+        
+        dp[0] = nums[0];
+        
+        for(int i=1;i<n;i++){
+            int left  = nums[i];
+            if(i>1) left+= dp[i-2];
+            int right = dp[i-1];
+            dp[i] = max(left, right);
+        }
+        return dp[n-1];
     }
 };
