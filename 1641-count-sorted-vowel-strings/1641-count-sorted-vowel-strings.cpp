2399223f57d98f2ea4ce150517227dff6d0fafc7
Mridul Bagoria <64253896+Mridul091@users.noreply.github.com>
2022-05-11 20:07:27 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (58.60%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     
-    void helper(int n, vector<vector<char>>& ans, vector<char>& temp, vector<char>& vowel, int k){
+//     void helper(int n, vector<vector<char>>& ans, vector<char>& temp, vector<char>& vowel, int k){
         
-        if(temp.size()==n){
-            ans.push_back(temp);
-            return;
-        }
+//         if(temp.size()==n){
+//             ans.push_back(temp);
+//             return;
+//         }
         
-        for(int i=k;i<vowel.size();i++){
+//         for(int i=k;i<vowel.size();i++){
             
-            temp.push_back(vowel[i]);
-            helper(n, ans, temp, vowel, i);
-            temp.pop_back();  
-        }
-        return;
-    }
+//             temp.push_back(vowel[i]);
+//             helper(n, ans, temp, vowel, i);
+//             temp.pop_back();  
+//         }
+//         return;
+//     }
     
     int countVowelStrings(int n) {
-        vector<vector<char>> ans;
-        vector<char> temp;
-        vector<char> vowel = {'a', 'e', 'i', 'o', 'u'};
-        helper(n, ans, temp, vowel,0);
-        return ans.size();
+        return (n+4)*(n+3)*(n+2)*(n+1) / 24;
     }
 };
