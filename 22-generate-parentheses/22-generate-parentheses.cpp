407f74322750fd4467d9639f55940556778d13d4
Mridul Bagoria <64253896+Mridul091@users.noreply.github.com>
2022-05-20 19:06:14 +0530

    Time: 4 ms (71.39%), Space: 13.8 MB (46.60%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,35 +1,44 @@
 class Solution {
 public:
     
-    bool isvalid(string& s){
-        int diff =0;
-        for(auto it: s){
-            if(it=='(')
-                diff= diff+1;
-            else 
-                diff = diff-1;
-            if(diff<0) return false;
-        }
+//     bool isvalid(string& s){
+//         int diff =0;
+//         for(auto it: s){
+//             if(it=='(')
+//                 diff= diff+1;
+//             else 
+//                 diff = diff-1;
+//             if(diff<0) return false;
+//         }
         
-        return !diff;
-    }
+//         return !diff;
+//     }
     
-    void helper(string s, int n, vector<string>& ans){
-        if(s.size()==n){
-            if(isvalid(s))
-                ans.push_back(s);
+//     void helper(string s, int n, vector<string>& ans){
+//         if(s.size()==n){
+//             if(isvalid(s))
+//                 ans.push_back(s);
+//             return;
+//         }
+        
+//         helper(s+'(', n, ans);
+//         helper(s+')', n, ans);
+        
+//     }
+    
+    void helper(int s, int e, int n, string temp, vector<string>& ans){
+        if(temp.size()==2*n){
+            ans.push_back(temp);
             return;
         }
         
-        helper(s+'(', n, ans);
-        helper(s+')', n, ans);
-        
+        if(s<n) helper(s+1, e, n, temp+'(', ans);
+        if(s>e) helper(s, e+1, n, temp+')', ans);
     }
-    
     
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        helper("", 2*n, ans);
+        helper(0,0,n,"", ans);
         return ans;
     }
 };
