985c55e12868aff4d40d41280de4c89594440541
Mridul Bagoria <64253896+Mridul091@users.noreply.github.com>
2022-05-21 07:43:05 +0530

    Added solution - LeetHub

--- Reach the Nth point - GFG/reach-the-nth-point.cpp
+++ Reach the Nth point - GFG/reach-the-nth-point.cpp
@@ -24,14 +24,17 @@ class Solution{
 		int nthPoint(int n){
 		    // Code here
 
-            vector<int> dp(n+1, 0);
             
-            dp[0] = 1;
-            dp[1] = 1;
+            
+            int prev = 1;
+            int curr = 1;
             for(int i=2;i<=n;i++){
-                dp[i] = (dp[i-1]+dp[i-2])%mod;
+                int curri = (prev + curr)%mod;
+                prev = curr;
+                curr=curri;
+                
 		    }
-            return dp[n];
+            return curr;
 		}
 };
 
