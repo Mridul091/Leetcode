e574bd3b050404b5ddbf22290bb1f47c114778f5
Mridul Bagoria <64253896+Mridul091@users.noreply.github.com>
2022-05-21 07:40:07 +0530

    Added solution - LeetHub

--- Reach the Nth point - GFG/reach-the-nth-point.cpp
+++ Reach the Nth point - GFG/reach-the-nth-point.cpp
@@ -5,24 +5,33 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
 	public:
-	    int mod = 1e9 + 7;
-	    int helper(int n, vector<int>& dp){
-	        if(n==0) return 1;
+	int mod = 1e9 + 7;
+	// memoization
+	    
+	   // int helper(int n, vector<int>& dp){
+	   //     if(n==0) return 1;
 		    
-		    if(dp[n]!=-1) return dp[n];
+		  //  if(dp[n]!=-1) return dp[n];
 		    
-		    int left = helper(n-1, dp);
-		    int right = 0;
+		  //  int left = helper(n-1, dp);
+		  //  int right = 0;
 		    
-		    if(n>=2) right = helper(n-2, dp);
+		  //  if(n>=2) right = helper(n-2, dp);
 		    
-		    return dp[n] = (left + right)%mod;
-	    }
+		  //  return dp[n] = (left + right)%mod;
+	   // }
 	    
 		int nthPoint(int n){
 		    // Code here
-		    vector<int> dp(n+1, -1);
-            return helper(n, dp);
+
+            vector<int> dp(n+1, 0);
+            
+            dp[0] = 1;
+            dp[1] = 1;
+            for(int i=2;i<=n;i++){
+                dp[i] = (dp[i-1]+dp[i-2])%mod;
+		    }
+            return dp[n];
 		}
 };
 
