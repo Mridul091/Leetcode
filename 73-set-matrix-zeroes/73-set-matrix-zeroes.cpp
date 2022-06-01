106063c67b459a6e52eef1b2ea818b5489ef8e8c
Mridul Bagoria <64253896+Mridul091@users.noreply.github.com>
2022-06-01 06:37:33 +0530

    Time: 20 ms (52.03%), Space: 13.6 MB (11.55%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        vector<vector<int>> dup(n, vector<int>(m,0));
+        //taking a m*n matrix and storing the value of the given matrix in dupliucate
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++)
+                dup[i][j] = matrix[i][j];
+        }
+        
+        //taversing through the duplicate matrix and making required changes in the given matrix 2d vector
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(dup[i][j]==0){
+                    int xl = i;
+                    int xu = i;
+                    int yl = j;
+                    int yr = j;
+                    while(xu>=0){
+                        matrix[xu][j] = 0;
+                        xu--;
+                    }
+                    while(yl>=0){
+                        matrix[i][yl] = 0;
+                        yl--;
+                    }
+                    while(xl<n){
+                        matrix[xl][j] = 0;
+                        xl++;
+                    }
+                    while(yr<m){
+                        matrix[i][yr] = 0;
+                        yr++;
+                    }
+                }
+            }
+        }
+    }
+};
