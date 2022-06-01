63342097288c8ba22c2eb1b2e9ef0cc39e720cb0
Mridul Bagoria <64253896+Mridul091@users.noreply.github.com>
2022-06-01 17:20:17 +0530

    Time: 60 ms (38.29%), Space: 6.4 MB (96.11%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
     
-    int helper(string &s, string &t, int i , int j, vector<vector<int>> &dp){
-        if(j<0) return 1;
-        if(i<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]) return dp[i][j] = helper(s,t,i-1,j-1, dp) + helper(s,t,i-1,j, dp);
-        else
-            return dp[i][j] = helper(s,t,i-1,j, dp);
-    }
+    // int helper(string &s, string &t, int i , int j, vector<vector<int>> &dp){
+    //     if(j<0) return 1;
+    //     if(i<0) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(s[i]==t[j]) return dp[i][j] = helper(s,t,i-1,j-1, dp) + helper(s,t,i-1,j, dp);
+    //     else
+    //         return dp[i][j] = helper(s,t,i-1,j, dp);
+    // }
     
     int numDistinct(string s, string t) {
         int s1 = s.size();
         int s2 = t.size();
-        vector<vector<int>> dp(s1, vector<int>(s2,-1));
-        return helper(s, t, s1-1, s2-1, dp);
+        vector<double> prev(s2+1, 0), curr(s2+1, 0); 
+        
+        prev[0] = curr[0] = 1;
+        for(int i=1;i<=s1;i++){
+            for(int j=1;j<=s2;j++){
+                if(s[i-1]==t[j-1])
+                    curr[j] = prev[j-1]  + prev[j];
+                else
+                    curr[j] = prev[j];
+            }
+            prev = curr;
+        }
+        return (int)prev[s2];
     }
 };
