3e07a38f6a60675364427abfc279f2c9a506d571
Mridul Bagoria <64253896+Mridul091@users.noreply.github.com>
2022-06-08 07:02:36 +0530

    Time: 11 ms (19.47%), Space: 10.7 MB (75.66%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -11,27 +11,37 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-    if(head==NULL)
-        return head;
-    ListNode *temp = head;
-        int cnt=0;
-        while(temp){
-            temp = temp->next;
-            cnt++;
+//     if(head==NULL)
+//         return head;
+//     ListNode *temp = head;
+//         int cnt=0;
+//         while(temp){
+//             temp = temp->next;
+//             cnt++;
+//         }
+//         if(cnt==1) return NULL;
+//         if(cnt==n) return head->next;
+//         int cnt1 = cnt-n-1;
+
+//         ListNode *temp1  = head;
+//         while(cnt1){
+//             temp1 = temp1->next;
+//             cnt1--;
+//         }
+//         temp1->next = temp1->next->next;
+//         return head;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        
+        while(n--){
+            fast = fast->next;
         }
-        if(cnt==1) return NULL;
-        if(cnt==n) return head->next;
-        int cnt1 = cnt-n-1;
-        cout << cnt << " ";
-        cout << cnt1 << " ";
-        ListNode *temp1  = head;
-        while(cnt1){
-            temp1 = temp1->next;
-            cnt1--;
+        if(!fast) return head->next;
+        while(fast->next){
+            slow = slow->next;
+            fast = fast->next;
         }
-        cout << cnt1 << " ";
-        cout << temp1-> val;
-        temp1->next = temp1->next->next;
+        slow->next = slow->next->next;
         return head;
     }
 };
