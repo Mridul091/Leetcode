88921892f26cbcaa30ac36c82e2d426a67dc88cd
Mridul Bagoria <64253896+Mridul091@users.noreply.github.com>
2022-06-30 07:19:17 +0530

    Added solution - LeetHub

--- Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.cpp
@@ -21,18 +21,20 @@ class Solution
     int longestSubsequence(int n, int a[])
     {
        // your code here
-       vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+    //   vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+       vector<int> curr(n+1,0), next(n+1,0);
     //   for(int i=0;i<=n;i++)
     //         dp[n][i] = 0;
         for(int i=n-1;i>=0;i--){
             for(int j =i-1;j>=-1;j--){
-                int len = 0 + dp[i+1][j+1];
+                int len = 0 + next[j+1];
                 if(j==-1 || a[i]>a[j])
-                    len = max(len, 1 + dp[i+1][i+1]);
-                dp[i][j+1] = len;
+                    len = max(len, 1 + next[i+1]);
+                curr[j+1] = len;
             }
+            next = curr;
         }
-       return dp[0][0];
+       return next[0];
     }
 };
 
