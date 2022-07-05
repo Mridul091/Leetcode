4ebdbcbc2a46c2d10566b9e8fbb5a0ab09491c6d
Mridul Bagoria <64253896+Mridul091@users.noreply.github.com>
2022-07-05 07:31:57 +0530

    Time: 34 ms (62.44%), Space: 13.6 MB (60.43%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,31 +1,45 @@
 class Solution {
 
 public: 
-    bool bipartite(int node, vector<vector<int>> adj,vector<int> &color ){
-    if(color[node]==-1) color[node] = 1;
+//     bool bipartite(int node, vector<vector<int>> adj,vector<int> &color ){
+//     if(color[node]==-1) color[node] = 1;
 
-    for(auto it : adj[node]){
-        if(color[it]==-1){
-            color[it] = 1- color[node];
-            if(!bipartite(it, adj, color)) return false;
-        }else if(color[it]==color[node]) return false;
-    }
-    return true;
-}
+//     for(auto it : adj[node]){
+//         if(color[it]==-1){
+//             color[it] = 1- color[node];
+//             if(!bipartite(it, adj, color)) return false;
+//         }else if(color[it]==color[node]) return false;
+//     }
+//     return true;
+// }
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        vector<vector<int>> adj(graph.size()+1);
-        
-        for(int i=0;i<graph.size();i++)
-        {   for(int k=0;k<graph[i].size();k++)
-             adj[i].push_back(graph[i][k]);  
+    
+    bool bipartite(int src, vector<vector<int>>& graph, vector<int>& color){
+        queue<int> q;
+        q.push(src);
+        color[src] = 0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(auto it: graph[node]){
+                if(color[it] == -1){
+                    color[it] = 1 - color[node];
+                    q.push(it);
+                }
+                else if(color[node]==color[it])return false;
+             }
         }
+        return true;
+    }
+    
+    bool isBipartite(vector<vector<int>>& graph) {
         
-    vector<int>color(graph.size()+1, -1);
+    int v = graph.size();
+    vector<int>color(v, -1);
  
-    for(int i = 0;i<graph.size();i++){
+    for(int i = 0;i<v;i++){
         if(color[i] == -1){
-            if(!bipartite(i, adj, color)){
+            if(!bipartite(i, graph, color)){
                 return false;
             }
         }
