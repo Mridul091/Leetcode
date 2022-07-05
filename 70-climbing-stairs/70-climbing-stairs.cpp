829ca501fed907b569d608aeaba44497e9f6d233
Mridul Bagoria <64253896+Mridul091@users.noreply.github.com>
2022-07-05 23:00:01 +0530

    Time: 5 ms (11.67%), Space: 6 MB (56.35%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -10,13 +10,15 @@ public:
 //     }
     
     int climbStairs(int n) {
-        vector<int > dp(n+1, 0);
-        dp[0] = 1;
-        dp[1] = 1;
+        
+        int prev2 = 1;
+        int prev = 1;
         // dp[2] = 1;
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int steps = prev2 + prev;
+            prev2 = prev;
+            prev = steps;
         }
-        return  dp[n];
+        return  prev;
     }
 };
