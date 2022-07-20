01e34ce58c2a776cbe2382382fe4c896651dd90f
Mridul Bagoria <64253896+Mridul091@users.noreply.github.com>
2022-07-20 20:37:06 +0530

    Time: 3 ms (80.67%), Space: 12.1 MB (76.26%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,36 +12,33 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        stack<TreeNode*> s1, s2;
+        
         
         vector<vector<int>> ans;
         
-        if(root==NULL) return ans;
-        s1.push(root);
-        while(!s1.empty()){
-           vector<int> v;
-            while(!s1.empty()){
-                TreeNode* temp = s1.top();
-                s1.pop();
-                v.push_back(temp->val);
-                if(temp->left) s2.push(temp->left);
-                if(temp->right) s2.push(temp->right);
-            }
-            if(v.size()>0)
-                ans.push_back(v);
+        if(root==NULL) return ans; 
+        queue<TreeNode*> q;
+        q.push(root);
+        bool flag = true;
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> temp(size);
             
-            vector<int> v1;
-            while(!s2.empty()){
-                TreeNode* temp = s2.top();
-                s2.pop();
+            for(int i =0;i<size;i++){
+                TreeNode* front = q.front();
+                q.pop();
                 
-                v1.push_back(temp->val);
-                if(temp->right) s1.push(temp->right);
-                if(temp->left) s1.push(temp->left);
+                int ind = flag ? i : size-i-1;
+                temp[ind] = front->val;
+                if(front->left)
+                    q.push(front->left);
+                if(front->right)
+                    q.push(front->right);
             }
-            if(v1.size()>0)
-                ans.push_back(v1);
+            flag = !flag;
+            ans.push_back(temp);
         }
+        
         
         return ans;
     }
