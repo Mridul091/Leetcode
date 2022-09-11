e07eeb9edf63938ba14504339732d8346cdfd5db
Mridul Bagoria <64253896+Mridul091@users.noreply.github.com>
2022-09-11 13:54:00 +0530

    Time: 234 ms (27.74%), Space: 23.4 MB (32.34%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -2,27 +2,50 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
-        if(n<3)
-            return {};
+//         if(n<3)
+//             return {};
         sort(nums.begin(), nums.end());
-        if(nums[0]>0)
-            return {};
-        unordered_map<int, int> mp;
-        vector<vector<int>> ans;
-        for(int i=0;i<n;i++){
-            mp[nums[i]] = i;
-        }
+//         if(nums[0]>0)
+//             return {};
+//         unordered_map<int, int> mp;
+//         vector<vector<int>> ans;
+//         for(int i=0;i<n;i++){
+//             mp[nums[i]] = i;
+//         }
         
-        for(int i =0;i<n-2;i++){
-            for(int j =i+1;j<n-1;j++){
-                int temp = nums[i] + nums[j];
-                if(mp.find(-temp)!=mp.end() && mp.find(-temp)->second > j){
-                    ans.push_back({nums[i], nums[j], -temp});
+//         for(int i =0;i<n-2;i++){
+//             for(int j =i+1;j<n-1;j++){
+//                 int temp = nums[i] + nums[j];
+//                 if(mp.find(-temp)!=mp.end() && mp.find(-temp)->second > j){
+//                     ans.push_back({nums[i], nums[j], -temp});
+//                 }
+//                 j = mp.find(nums[j])->second;
+//             }
+//             i = mp.find(nums[i])->second;
+//         }
+//         return ans;
+        
+        vector<vector<int>> res;
+        for(int i = 0;i<n-2;i++){
+            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
+                int lo = i+1, hi = n-1, target = -nums[i];
+                while(lo < hi){
+                    if(nums[lo] + nums[hi] == target){
+                        vector<int> temp;
+                        temp.push_back(nums[i]);
+                        temp.push_back(nums[lo]);
+                        temp.push_back(nums[hi]);
+                        res.push_back(temp);
+                        
+                        while(lo < hi && nums[lo]==nums[lo+1]) lo++;
+                        while(lo < hi && nums[hi]==nums[hi-1]) hi--;
+                        lo++, hi--;
+                    }
+                    else if(nums[lo] + nums[hi] < target) lo++;
+                    else hi--;
                 }
-                j = mp.find(nums[j])->second;
             }
-            i = mp.find(nums[i])->second;
         }
-        return ans;
+        return res;
     }
 };
